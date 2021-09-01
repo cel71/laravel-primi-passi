e875c4929f11659e459975e20599581ecbcdb732
cel71 <celestialc71@libero.it>
2021-09-01 16:21:05 +0200

    laravel-primi-passi es2

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', 
+        ['text' => 'Benvenuto']
+    );
+});
+
+Route::get('/info', function () {
+    return view('info', 
+        ['text2' => 'Qui puoi trovare le informazioni che ti servono']
+    );
+});
+
+Route::get('/maps', function () {
+    return view('maps', 
+        ['src' => 'https://www.itala.it/images/site_blog_art/5/image/zoom_mappa_google_maps.jpg?box=770x1000']
+    );
+});
+
+Route::get('/contatti', function () {
+    return view('contatti', 
+        ['text3' => 'Come entrare in contatto con noi:']
+    );
 });
